e243634a3f9abd553f6fb92ed7cb4e6fb06a1192
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-24 15:02:30 +0530

    added tuple

---
+++ 004_tuple.py
@@ -0,0 +1,23 @@
+# TUPLE
+# (), ordered, index, allowed duplicates, non-mutable
+
+my_tuple = (1, 3, 2, 5, 6, 5)
+
+# access first element
+print(my_tuple[0])
+
+# access second element
+print(my_tuple[1])
+
+# access first three elements
+print(my_tuple[0:3:1])
+
+# access only first and third element
+print(my_tuple[0:3:2])
+
+# tuple print
+print(my_tuple)
+
+# # first element replace 1000
+# my_tuple[0] = 1000
+# print(my_tuple)
