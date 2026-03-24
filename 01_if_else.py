dcf8d994af9c4eacfde13e20743e40043546ce10
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-24 15:14:35 +0530

    if else block

---
+++ 01_if_else.py
@@ -0,0 +1,21 @@
+# if else
+
+# if block
+my_age = 18
+if my_age > 18:
+    print("Adult")
+
+# if else block
+if my_age > 18:
+    print("Adult")
+else:
+    print("Minor")
+
+# if elif else block
+if my_age > 18:
+    print("Adult")
+elif my_age == 18:
+    print("Turing Adult")
+else:
+    print("Minor")
+
