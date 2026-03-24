cd0fe14ea19dc773c25b15e2b9e3efddb400393b
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-24 14:54:30 +0530

    added dict

---
+++ 003_dict.py
@@ -0,0 +1,26 @@
+#DICT
+# {}, keys, duplicates not allowed, ordered, mutable
+
+
+my_dict = {
+    "Name":"Rohan",
+    "Age": 35,
+    "Gender":"Male",
+    "Name":"Sohan"
+}
+
+# call 'Rohan'
+print(my_dict['Name'])
+
+# get all keys
+print(my_dict.keys())
+
+# get all values
+print(my_dict.values())
+
+# get both key value pair
+print(my_dict.items())
+
+# change value of age
+my_dict["Age"] = 25
+print(my_dict)
